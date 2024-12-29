a6bf9cbe113f7d0a40c76038b7cf706d43c0245b
brahimsamih <134377321+brahimsamih@users.noreply.github.com>
2024-12-29 14:11:02 +0100

    flutter

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/components/my_drawer.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void logout() {
+    //get auth service
+    final _auth = AuthService();
+    _auth.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        actions: [
+          //logout button
+          IconButton(onPressed: logout, icon: Icon(Icons.logout))
+        ],
+      ),
+      drawer: MyDrawer(),
+    );
+  }
+}
---
+++ lib/services/chat/chat_service.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatService {
+  //get instance of firebase
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  // get user stream
+  /*
+  List<Map<String,dynamic> = 
+
+  {
+  email: test@gmail.com
+  'id': ..
+  }
+  */
+  Stream<List<Map<String, dynamic>>> getUserStream() {
+    return _firestore.collection("Users").snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        final user = doc.data();
+
+        return user;
+      }).toList();
+    });
+  }
+  // send message
+
+  // get message
+}
